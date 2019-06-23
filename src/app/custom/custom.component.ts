4c3604c5ddaccb9e5e9d79ecf1d967e7db4e0592
Ripon Ghosh <ripon.ghosh200@gmail.com>
2019-06-23 16:51:39 +0600

    custom loader make

--- src/app/custom/custom.component.ts
+++ src/app/custom/custom.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-custom',
-  templateUrl: './custom.component.html',
-  styleUrls: ['./custom.component.css']
+	selector: 'app-custom',
+	templateUrl: './custom.component.html',
+	styleUrls: ['./custom.component.css']
 })
 export class CustomComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	public numbers=[];
+	public loading = false;
+
+	ngOnInit() {
+		this.generateArray();
+	}
+
+
+	async generateArray(){
+		if(this.loading) return;
+		this.loading=true;
+		let len=this.numbers.length;
+		for(let i=len;i<=len+50;i++){
+			await this.sleep(500).then(() => {
+				this.numbers.push(i);
+			})
+		}
+		this.loading=false;
+	}
+
+	sleep = (milliseconds) => {
+		return new Promise(resolve => setTimeout(resolve, milliseconds))
+	}
 
 }
