bf4f990c40a8704190f84c616b659563c1a9f7f8
Ripon Ghosh <ripon.ghosh200@gmail.com>
2019-06-23 13:00:56 +0600

    ngx-spinner install

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NgxLoadingComponent } from './ngx-loading/ngx-loading.component';
+import { NgxSpinnerComponent } from './ngx-spinner/ngx-spinner.component';
 
 const routes: Routes = [
 	{path: 'ngx-loading', component: NgxLoadingComponent},
+	{path: 'ngx-spinner', component: NgxSpinnerComponent},
 ];
 
 @NgModule({
